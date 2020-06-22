ea9104579af0a2bf8b0880cb1b845f2347a6ef79
mhvarner <62973622+mhvarner@users.noreply.github.com>
2020-06-21 22:43:38 -0400

    Final Updates to page deployment.

--- plots.js
+++ plots.js
@@ -61,7 +61,7 @@ function buildCharts(sample) {
         marker: {
           size: sample_values,
           color: otu_ids,
-          colorscale: "Earth"
+          colorscale: "YlOrRd"
         }
       }
     ];
